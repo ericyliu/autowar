fac51e8e768da49d2ba50231e13dd0f10b595c62
Eric Liu <eyliubit@gmail.com>
2019-06-21 10:22:14 -0400

    add camera slider

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+  public GameObject cameraRig;
+  public Slider cameraSlider;
+  public float cameraMaxX = 170f;
+  // Start is called before the first frame update
+  void Start()
+  {
+    cameraSlider.onValueChanged.AddListener(delegate { OnSliderChange(); });
+  }
+
+  public void OnSliderChange()
+  {
+    cameraRig.transform.position = new Vector3(
+      cameraSlider.value * this.cameraMaxX,
+      cameraRig.transform.position.y,
+      cameraRig.transform.position.z
+    );
+  }
+}
